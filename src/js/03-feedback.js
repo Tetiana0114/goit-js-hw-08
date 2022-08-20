f7c57619320c3c615516155933e00d1ed56390bf
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-20 03:07:28 +0300

    Task 03 in process

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onFormInput, 500));
+
+// storageDataChecking();
+
+// Зберігаємо об'єкт зі значеннями email, message у локальне сховище
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// function storageDataChecking(event) {
+//     const savedData = localStorage.getItem(STORAGE_KEY);
+//     if (savedData) {
+//       
+//     }  
+// }
+
+// Очищуємо поля форми та сховище, виводимо об'єкт даних в консоль
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
 
