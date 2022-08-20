aee9b8410464767c97540c4e3a50460ba2b9a901
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-21 02:13:51 +0300

    Task 02-video in process

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+const STORAGE_KEY = "videoplayer-current-time";
+
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.on('timeupdate', function(event) {
+    //  console.log('time', event);
+     localStorage.setItem(STORAGE_KEY, event.seconds);
+    });
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
 
